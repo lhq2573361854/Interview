9ef2eec470784d088bc912c5b79630ea329d196f
admin <adminadmin@163.com>
2019-04-06 18:44:53 +0800

    排序算法

--- stdsort.h
+++ stdsort.h
@@ -43,6 +43,10 @@ public:
     void _siftup(int index);
     void _siftDown(int index);
     int extractMax();
+
+    int maxbit();
+    void radixsort();
+
 	//相当于javaBean
     int getWidth();
     void setWidth(int n);
@@ -222,7 +226,48 @@ void Sort::heapSort() {
     }
 }
 
+int Sort::maxbit()
+{
+    int d = 1; //保存最大的位数
+    int p = 10;
+    for(int i = 0; i < n; ++i)
+    {
+        while(arr[i] >= p)
+        {
+            p *= 10;
+            ++d;
+        }
+    }
+    return d;
+}
+void Sort::radixsort(){
+    int d = maxbit();
+    int tmp[n];
+    int count[10]; //计数器
+    int i, j, k;
+    int radix = 1;
+    for (i = 1; i <= d; i++) //进行d次排序
+    {
+        for (j = 0; j < 10; j++)
+            count[j] = 0; //每次分配前清空计数器
+        for (j = 0; j < n; j++) {
+            k = (data[j] / radix) % 10; //统计每个桶中的记录数
+            count[k]++;
+        }
+        for (j = 1; j < 10; j++)
+            count[j] = count[j - 1] + count[j]; //将tmp中的位置依次分配给每个桶
+        for (j = n - 1; j >= 0; j--) //将所有桶中记录依次收集到tmp中
+        {
+            k = (arr[j] / radix) % 10;
+            tmp[count[k] - 1] = arr[j];
+            count[k]--;
+        }
+        for (j = 0; j < n; j++) //将临时数组的内容复制到data中
+            arr[j] = tmp[j];
+        radix = radix * 10;
+    }
 
+}
 
 void Sort::print() {
     for (int i = 0; i < n; ++i) {
